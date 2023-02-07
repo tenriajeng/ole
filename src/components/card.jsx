f86937d9042b77bfdeef2faf89948aba27e8d68d
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-02-07 10:39:03 +0800

    Update card.jsx

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,11 +5,11 @@ function Card({ topic }) {
 	const PlaceholderImage = "https://sejawat.s3.ap-southeast-1.amazonaws.com/sejawat/avatars/4a8820cc6507849820d03deae55af243/adventurer.png";
 
 	return (
-		<div className="border-solid border border-indigo-600 w-full rounded-xl mb-2 overflow-hidden">
-			<LazyLoadImage src={topic.image} className="w-full" alt={topic.slug} PlaceholderSrc={PlaceholderImage} effect="blur" />
-			<div className="p-2">
+		<div className="border-solid border border-indigo-600 w-full aspect-square rounded-xl mb-2 overflow-hidden">
+			<LazyLoadImage src={topic.image} className="w-full" alt={topic.slug} PlaceholderSrc={PlaceholderImage} effect="blur" width="640" height="640" />
+			{/* <div className="p-2">
 				<h1 className="">{topic.name}</h1>
-			</div>
+			</div> */}
 		</div>
 	);
 }
