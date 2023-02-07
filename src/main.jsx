150791b2c0a4843f29a92bc737f62f863a67a916
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-02-07 13:54:10 +0800

    add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<HelmetProvider>
 			<App />
 		</HelmetProvider>
+		<Analytics mode={"production"} />;
 	</React.StrictMode>
 );
