109296de37a8f610831f886421a1f0ac661c05df
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-02-07 10:08:36 +0800

    alt image

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,7 @@
 function Card({ topic }) {
 	return (
 		<div className="border-solid border border-indigo-600 w-full rounded-xl mb-2 overflow-hidden">
-			<img src={topic.image} className="w-full" />
+			<img src={topic.image} className="w-full" alt={topic.slug} />
 			<div className="p-2">
 				<h1 className="">{topic.name}</h1>
 			</div>
