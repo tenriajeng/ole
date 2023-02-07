66504eb7c535f3b688a0e8758725f01b9ae521ac
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-02-07 10:13:42 +0800

    Update navigation.jsx

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -8,7 +8,9 @@ function Navigation() {
 		<div className="h-16 bg-indigo-50 w-screen flex items-center justify-center">
 			<div className="w-11/12 flex justify-between">
 				<div className="w-2/12">
-					<a className={brandStyle}>Sejawat</a>
+					<a href="/" className={brandStyle}>
+						Sejawat
+					</a>
 				</div>
 				<div className="w-10/12 flex justify-end space-x-10 items-center">
 					<a href="/home" className={menuStyle}>
