a4933770c9e65ac96ea59a946268768f1aca2425
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-02-07 10:26:30 +0800

    Update navigation.jsx

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Navigation() {
-	const menuStyle = "font-normal text-indigo-500 hover:text-blue-500 hover:cursor-pointer";
-	const brandStyle = "text-3xl font-semibold text-indigo-500 hover:text-blue-500 hover:cursor-pointer";
+	const menuStyle = "font-semibold text-white hover:cursor-pointer";
+	const brandStyle = "text-3xl font-semibold text-white hover:cursor-pointer";
 
 	return (
-		<div className="h-16 bg-indigo-50 w-screen flex items-center justify-center">
+		<div className="h-16 bg-indigo-600 w-screen flex items-center justify-center">
 			<div className="w-11/12 flex justify-between">
 				<div className="w-2/12">
 					<a href="/" className={brandStyle}>
