2f5589982dbfd51255d30d7fed60ce9d80a99f01
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-02-07 10:33:32 +0800

    add lazy load image

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,12 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+
 function Card({ topic }) {
+	const PlaceholderImage = "https://sejawat.s3.ap-southeast-1.amazonaws.com/sejawat/avatars/4a8820cc6507849820d03deae55af243/adventurer.png";
+
 	return (
 		<div className="border-solid border border-indigo-600 w-full rounded-xl mb-2 overflow-hidden">
-			<img src={topic.image} className="w-full" alt={topic.slug} />
+			<LazyLoadImage src={topic.image} className="w-full" alt={topic.slug} PlaceholderSrc={PlaceholderImage} effect="blur" />
 			<div className="p-2">
 				<h1 className="">{topic.name}</h1>
 			</div>
